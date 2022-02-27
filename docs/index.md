c796eecce3c2ff414ae5b84d7d90d0f3ef990d7c
Luke <sabolukas03@gmail.com>
2022-02-27 13:28:51 +0100

    Initial commit

---
+++ docs/index.md
@@ -0,0 +1,8 @@
+---
+title: Documentation
+---
+
+Here you can find all of the official documentation for resoruces developed by the Overextended team
+
+If you feel like the documentation is lacking in some department then feel free to go onto that page
+and click the "Edit this page" at the bottom and submit a Pull Request.
---
+++ docusaurus.config.js
@@ -0,0 +1,70 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Overextended',
+  tagline: 'Official documentation for Overextended resources',
+  url: 'https://overextended.github.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/ox.ico',
+  organizationName: 'overextended', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true
+      },
+      navbar: {
+        title: 'Overextended',
+        logo: {
+          alt: 'Ox Logo',
+          src: 'img/ox.png',
+          width: 32,
+          height: 32,
+        },
+        items: [
+          {
+            to: 'docs/',
+            label: 'Documentation',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Overextended`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
