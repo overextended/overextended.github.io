ca9ea6be4efba4e11a14fa0a4333ea61ef532c44
Luke <sabolukas03@gmail.com>
2022-02-27 15:21:08 +0100

    Add media links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,16 +44,31 @@ const config = {
         title: 'Overextended',
         logo: {
           alt: 'Ox Logo',
-          src: 'img/ox.png',
+          src: 'img/ox_64.png',
           width: 32,
           height: 32,
         },
         items: [
           {
+            href: "https://www.github.com/overextended",
+            label: 'GitHub',
+            position: 'left'
+          },
+          {
+            href: "https://www.discord.gg/overextended",
+            label: 'Discord',
+            position: 'left'
+          },
+          {
             to: 'docs/',
             label: 'Documentation',
             position: 'right',
           },
+          {
+            // Still have to add the actual search :(
+            type: 'search',
+            position: 'right'
+          }
         ],
       },
       footer: {
