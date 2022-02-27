3841659fa8d2c6777f66cb6c42743aa1ccdfdee7
Luke <sabolukas03@gmail.com>
2022-02-27 14:04:06 +0100

    Proper ox image path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ function HomepageHeader() {
   return (
     <div className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className={`container ${styles.container}`}>
-        <img src="/img/ox.png" alt="ox" width={256} height={256} className={styles.ox} />
+        <img src="img/ox.png" alt="ox" width={256} height={256} className={styles.ox} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
       </div>
