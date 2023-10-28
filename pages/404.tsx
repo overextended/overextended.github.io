4e4a4b0891c9882d89a4b1c623525e9a412753f3
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-10-28 19:09:49 +0200

    refactor: improve 404 page look

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,12 +5,16 @@ const FourOhFour: React.FC = () => {
   const router = useRouter();
 
   React.useEffect(() => {
-    router.push('/');
+    const timeout = setTimeout(() => router.push('/'), 1500);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
-    <div className="flex h-full w-full items-center justify-center bg-neutral-900">
+    <div className="flex h-full w-full flex-col items-center justify-center bg-neutral-900">
+      <img src="/static/ox.ico" width="64" />
       <p className="text-4xl text-white">404</p>
+      <p className="text-lg text-white">Redirecting...</p>
     </div>
   );
 };
