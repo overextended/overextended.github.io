d2a1643f00697eccf3993a69c6f6acb171519529
Linden <65407488+thelindat@users.noreply.github.com>
2025-01-22 01:36:38 +1100

    update our team

---
+++ components/DisplayMembers.tsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from 'react'
+import MemberLink from './MemberLink';
+
+interface GithubMember {
+  login: string;
+  avatar_url: string;
+}
+
+interface MemberData {
+  data: GithubMember[];
+  etag: string | null;
+  timestamp: number;
+}
+
+const cacheKey = 'overextendedMembers';
+const cacheExpiry = 24 * 60 * 60 * 1000;
+
+const sponsor = {
+  thelindat: `thelindat`,
+  LukeWasTakenn: `lukewastaken`,
+  DokaDoka: `dokadoka`,
+}
+
+async function FetchMembers() {
+  const raw = localStorage.getItem(cacheKey);
+  const members: MemberData = raw ? JSON.parse(raw) : {};
+  const isCacheExpired = members.timestamp ? new Date().getTime() - members.timestamp > cacheExpiry : true;
+
+  if (!isCacheExpired) return members;
+
+  const headers = {}
+
+  if (members.etag) headers['If-None-Match'] = members.etag;
+
+  const response = await fetch(`https://api.github.com/orgs/overextended/members`, {
+      headers: headers
+  })
+
+  if (response.status === 304) return members;
+
+  members.data = await response.json();
+  members.etag = response.headers.get('ETag');
+  members.timestamp = new Date().getTime();
+
+  localStorage.setItem(cacheKey, JSON.stringify(members));
+  console.log(`fetched data and set cache (etag ${members.etag})`, members.timestamp)
+  
+  return members;
+}
+
+const DisplayMembers = () => {
+  const [members, setMembers] = useState(null);
+
+  useEffect(() => {
+    (async() => setMembers(await FetchMembers()))()
+  }, [])
+
+  //@ts-ignore ???
+  return (members?.data ? <div className="grid md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 grid-cols-1 mt-4 gap-16">
+    {members.data
+        .map((member) => MemberLink({ image: member.avatar_url, name: member.name ?? member.login, sponsor: sponsor[member.login] }))}
+  </div> : "")
+};
+
+export default DisplayMembers;
---
+++ components/MemberLink.tsx
@@ -0,0 +1,24 @@
+import { IconCoffee } from '@tabler/icons-react';
+
+const MemberLink: React.FC<{ image: string; name: string; sponsor?: string }> = ({ image, name, sponsor }) => {
+  return (
+    <div key={name} className="flex w-32 flex-col items-center justify-center gap-2">
+      <a href={`https://github.com/${name}`} target="_blank" className="flex flex-col items-center gap-2">
+        <p className="text-sky-500 font-semibold">{name}</p>
+        <img src={image} alt={`${name}-image`} className="rounded-md bg-neutral-100 shadow-md dark:bg-neutral-900" />
+      </a>
+      <a href={sponsor ? `https://ko-fi.com/${sponsor}` : ''} target="_blank" className="w-full">
+        <button className={`flex w-full items-center justify-center gap-2 rounded-md p-2 ${sponsor
+          ? "text-sky-500 bg-sky-500/20 hover:bg-sky-500/30"
+          : "text-slate-500 bg-slate-500/20"
+        }`}>
+          <IconCoffee />
+          Support
+        </button>
+      </a>
+    </div>
+  );
+};
+
+
+export default MemberLink;
