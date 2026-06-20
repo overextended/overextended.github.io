92e09cb454f469451c3d4a563bb1ebbcfaec7097
Linden <65407488+thelindat@users.noreply.github.com>
2026-06-20 22:39:23 +1000

    properly align kofi icon to centre

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col justify-center text-center flex-1 prose">
+    <div className="flex flex-col justify-center text-center flex-1 prose items-center">
       <h1 className="text-2xl font-bold">Welcome to Overextended</h1>
 
       <a href="https://ko-fi.com/thelindat">
-        <img src="/static/kofi.png" className="w-40 m-auto" />
+        <img src="/static/kofi.png" className="w-40" />
       </a>
 
       <Link href="/docs">Continue to Documentation →</Link>
